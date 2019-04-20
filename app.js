dc96f27fa31f8d68c524b2559024e3c0ede5c3b2
vdupati <vdupati@outlook.com>
2019-04-20 22:11:59 +0530

    added gdrive module

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var usersRouter = require('./routes/users');
+var gDriveRouter = require('./routes/gDrive');
 
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/users', usersRouter);
+app.use('/gDrive', gDriveRouter);
 
 module.exports = app;
